19553536056c7ef3caa2a55a116d147a54046bf0
Tabitha Rollins <tabitha@keyneston.com>
2020-10-28 17:36:22 +0100

    Diff now gets the changeset and prints it out (ugly like)

--- cmds/diff/diff.go
+++ cmds/diff/diff.go
@@ -6,10 +6,15 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"sync"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/arn"
+	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/service/cloudformation"
 	"github.com/google/subcommands"
+	"github.com/keyneston/cftool/awshelpers"
 	"github.com/keyneston/cftool/config"
 	"github.com/keyneston/tabslib"
 )
@@ -23,6 +28,8 @@ var staticCapabilities = []*string{
 type DiffStacks struct {
 	General  *config.GeneralConfig
 	StacksDB *config.StacksDB
+
+	Timeout time.Duration
 }
 
 func (*DiffStacks) Name() string { return "diff" }
@@ -36,36 +43,141 @@ func (*DiffStacks) Usage() string {
 }
 
 func (r *DiffStacks) SetFlags(f *flag.FlagSet) {
+	f.DurationVar(&r.Timeout, "t", time.Second*60, "timeout for waiting for results")
 }
 
 func (r *DiffStacks) Execute(ctx context.Context, f *flag.FlagSet, _ ...interface{}) subcommands.ExitStatus {
+	if r.Timeout < time.Second {
+		log.Printf("Invalid timeout set: %v", r.Timeout)
+		return subcommands.ExitFailure
+	}
+	ctx, cancel := context.WithTimeout(ctx, r.Timeout)
+	defer cancel()
+
 	stacks, err := r.StacksDB.Filter(f.Args()...)
 	if err != nil {
 		log.Printf("Error: %v", err)
 		return subcommands.ExitFailure
 	}
 
+	changeSets := []string{}
+
 	for _, s := range stacks.All {
 		log.Printf("Diffing: %s", s.Name)
-		if err := r.createChangeSet(s); err != nil {
+		id, err := r.createChangeSet(s)
+		if err != nil {
 			log.Printf("Error: %v", err)
 			return subcommands.ExitFailure
 		}
+		if id == "" {
+			// If id is empty and there is no error then the there is nothing
+			// to change.
+			continue
+		}
+
+		changeSets = append(changeSets, id)
+	}
+
+	if err := r.getResults(ctx, changeSets); err != nil {
+		log.Printf("Error: %v", err)
+		return subcommands.ExitFailure
 	}
 
 	return subcommands.ExitSuccess
 }
 
-func (r *DiffStacks) createChangeSet(s *config.StackConfig) error {
+func (r *DiffStacks) getResults(ctx context.Context, ids []string) error {
+	errCh := make(chan error, len(ids))
+	resultCh := make(chan *cloudformation.DescribeChangeSetOutput, len(ids))
+	wg := &sync.WaitGroup{}
+	wg.Add(len(ids))
+
+	for _, id := range ids {
+		go r.waitForResult(ctx, wg, errCh, resultCh, id)
+	}
+
+	wg.Wait()
+
+	close(errCh)
+	close(resultCh)
+
+	for err := range errCh {
+		log.Printf("Error: %v", err)
+	}
+
+	for result := range resultCh {
+		log.Printf("Result: %v", result)
+	}
+
+	return nil
+}
+
+type logger struct{}
+
+func (logger) Log(stuff ...interface{}) {
+	log.Printf("Logging: %#v", stuff)
+}
+
+func (r *DiffStacks) waitForResult(ctx context.Context, wg *sync.WaitGroup, errCh chan<- error, results chan<- *cloudformation.DescribeChangeSetOutput, id string) {
+	defer wg.Done()
+
+	a, err := arn.Parse(id)
+	if err != nil {
+		errCh <- err
+		return
+	}
+
+	client := awshelpers.GetClient(a.Region)
+	input := &cloudformation.DescribeChangeSetInput{
+		ChangeSetName: &id,
+	}
+
+	log.Printf("%v", tabslib.PrettyString(input))
+
+	log.Printf("Waiting for changeset to calculate")
+	if err := client.WaitUntilChangeSetCreateCompleteWithContext(ctx, input,
+		request.WithWaiterDelay(request.ConstantWaiterDelay(time.Second)),
+		request.WithWaiterMaxAttempts(int(r.Timeout.Seconds())),
+		request.WithWaiterLogger(logger{}),
+	); err != nil {
+		err = fmt.Errorf("Error fetching changeset (may be due to no changes?): %v", err)
+		errCh <- err
+		return
+	}
+
+	log.Printf("Fetching changeset")
+	output, err := client.DescribeChangeSetWithContext(ctx, input)
+	if err != nil {
+		errCh <- err
+		return
+	}
+
+	results <- output
+}
+
+func (r *DiffStacks) createChangeSet(s *config.StackConfig) (string, error) {
 	template, err := s.GetDiskTemplate()
 	if err != nil {
-		return err
+		return "", err
+	}
+	templateHash, err := s.GetDiskTemplateHash()
+	if err != nil {
+		return "", err
+	}
+	liveHash, err := s.GetLiveTemplateHash()
+	if err != nil {
+		return "", err
+	}
+
+	// TODO: also check if any parameters change
+	if templateHash == liveHash {
+		return "", nil
 	}
 
 	stackName := s.StackName()
 	name, err := changesetName(s)
 	if err != nil {
-		return err
+		return "", err
 	}
 
 	changeSetInput := &cloudformation.CreateChangeSetInput{
@@ -77,23 +189,24 @@ func (r *DiffStacks) createChangeSet(s *config.StackConfig) error {
 		Parameters:    s.AWSParams(),
 	}
 	if err := changeSetInput.Validate(); err != nil {
-		return err
+		return "", err
 	}
-	log.Printf("Sending: %s", tabslib.PrettyString(changeSetInput))
 
-	//region, err := s.Region()
-	//if err != nil {
-	//	return err
-	//}
+	region, err := s.Region()
+	if err != nil {
+		return "", err
+	}
 
-	//client := awshelpers.GetClient(region)
-	//res, err := client.CreateChangeSet(changeSetInput)
-	//if err != nil {
-	//	return err
-	//}
-	//log.Printf("Res: %s", tabslib.PrettyString(res))
+	client := awshelpers.GetClient(region)
+	res, err := client.CreateChangeSet(changeSetInput)
+	if err != nil {
+		return "", err
+	}
 
-	return nil
+	if res.Id == nil {
+		return "", fmt.Errorf("No changeset ID returned from AWS")
+	}
+	return *res.Id, nil
 }
 
 func changesetName(s *config.StackConfig) (string, error) {
